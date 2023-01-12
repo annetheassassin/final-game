499b9c5961afa7374c54e09b51bb1bcf201d84db
annetheassassin <74738723+annetheassassin@users.noreply.github.com>
2023-01-12 12:19:57 +0100

    shopm

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public void PurchaseLevel1Turret (){
+        Debug.Log("Level1 Turret Purchased");
+    }
+    public void PurchaseLevel2Turret (){
+        Debug.Log("Level2 Turret Purchased");
+    }
+    public void PurchaseLevel3Turret (){
+        Debug.Log("Level3 Turret Purchased");
+    }
+    public void PurchaseLevel4Turret (){
+        Debug.Log("Level4 Turret Purchased");
+    }
+    public void PurchaseLevel5Turret (){
+        Debug.Log("Level5 Turret Purchased");
+    }
+}
