83326c50f3fb62b5d4fa41efa28d651e50b6e7b4
annetheassassin <74738723+annetheassassin@users.noreply.github.com>
2023-01-17 13:59:28 +0100

    help

--- Assets/shop1.cs
+++ Assets/shop1.cs
@@ -3,35 +3,35 @@ using UnityEngine;
 
 public class shop1 : MonoBehaviour 
 
-{   Buildmanager buildmanager;
-    void Start()
-    {
-        buildmanager = Buildmanager.instance;
+{  //Buildmanager buildmanager;
+   void Start()
+   {
+      // buildmanager = Buildmanager.instance;
 
-    }
+   }
     public void PurchasesintTurrut ()
     {
         Debug.Log("sint bought");
-        buildmanager.SetTurretToBuild(buildmanager.sintprefab);
+    //    buildmanager.SetTurretToBuild(buildmanager.sintprefab);
     }
    public void purchaseLevel1piet()
     {
         Debug.Log("pietlevel1 bought");
-        buildmanager.SetTurretToBuild(buildmanager.Level1piet);
+     //   buildmanager.SetTurretToBuild(buildmanager.Level1piet);
     }
     public void purchaseLevel2piet()
     {
         Debug.Log("pietlevel2 bought");
-        buildmanager.SetTurretToBuild(buildmanager.Level2piet);
+      //  buildmanager.SetTurretToBuild(buildmanager.Level2piet);
     }
     public void purchaseLevel3piet()
     {
         Debug.Log("pietlevel3 bought");
-        buildmanager.SetTurretToBuild(buildmanager.Level3piet);
+      //  buildmanager.SetTurretToBuild(buildmanager.Level3piet);
     }
     public void purchaseLevel4piet()
     {
         Debug.Log("pietlevel4 bought");
-        buildmanager.SetTurretToBuild(buildmanager.Level4piet);
+       // buildmanager.SetTurretToBuild(buildmanager.Level4piet);
     }
 }
