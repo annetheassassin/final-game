4c702c1c733cfcf712a07f8de64bc1c234c90dd2
annetheassassin <74738723+annetheassassin@users.noreply.github.com>
2023-01-24 13:11:11 +0100

    hop

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    private GameObject Tower;
+    buildmanager buildmanager;
+    public Color hovercolor;
+    private Color startcolor;
+    private Renderer rend;
+    void Start()
+    {
+        buildmanager = buildmanager.instance;
+        rend = Getcomponent<Renderer>();
+        startcolor = rend.sprite.color;
+    }
+    void OnMouseEnter ()
+    {
+       rend.sprite.color = hovercolor;
+
+    }
+    
+    void OnMouseDown()
+    {
+        Debug.Log("click");
+        if (buildmanager.GetTowerTobluid() == null) 
+        return; 
+
+        if(Tower != null)
+        {
+            Debug.Log("cant place here");
+            return;
+        }
+        GameObject TowerToBuilt = buildmanager.GetTowerTobluid();
+        Tower = (GameObject) Instantiate(TowerToBuilt, transform.position, transform.rotation);
+
+    }
+    void OnMouseExit()
+    {
+        rend.sprite.color = startcolor;
+    }
+}
